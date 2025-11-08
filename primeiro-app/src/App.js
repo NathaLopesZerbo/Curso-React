d9a9c8d846706fc60d17314814ab599ecc98f979
NathaLopesZerbo <nathalopeszerbo7@gmail.com>
2025-11-08 12:29:57 -0300

    Entendendo sobre a estutura de array, spread operator e map

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -2,64 +2,42 @@ import { useState } from 'react';
 
 //Getter/Setter
 export default function App() {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState(0);
-
-  const [user, setUser] = useState({})
+  const [input, setInput] = useState('')
+  const [tarefas, setTarefas] = useState([
+    
+  ]);
 
   function handleRegister(e){
     e.preventDefault();
 
-    alert('Usuario Registrado !');
-
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    });
-
+    //Spread Operator = Renderizar oq já está na listagem
+    setTarefas([...tarefas, input])
+    setInput('');
   }
 
  return (
    <div>
     <h1>Cadastre-se</h1>
      <form onSubmit={handleRegister}>
-        <label>Nome: </label>
+        <label>Nome da Tarefa: </label>
         <br />
           <input 
-            placeholder='Digite seu Nome:' 
-            value={nome}
-            onChange={ (e) => setNome(e.target.value) }
-          />
-        <br />
-
-        <label>Email:</label>
-        <br />
-          <input 
-            placeholder='Digite seu Email:' 
-            value={email}
-            onChange={ (e) => setEmail(e.target.value) }
-          />
-        <br />
-
-       <label>Idade: </label>
-       <br />
-          <input 
-            placeholder='Digite seu Idade:' 
-            value={idade}
-            onChange={ (e) => setIdade(e.target.value) }
+            placeholder='Digite sua Tarefa:' 
+            value={input}
+            onChange={ (e) => setInput(e.target.value) }
           />
         <br />
         <button type='submit'>Registar</button>
      </form>
 
      <br />
-     <div>
-        <span>Bem Vindo:{user.nome}</span> <br />
-        <span>Idade:{user.idade}</span> <br />
-        <span>Email:{user.email}</span> <br />
-     </div>
+
+     <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}> {tarefa}</li>
+        ))}
+     </ul>
+
    </div>
  );
 }
